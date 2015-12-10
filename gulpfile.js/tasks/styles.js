ce144a0be7bdc31a20a9cc8fae1ac965d94b3e1c
Fredrik Broman <frebro@gmail.com>
2015-12-10 07:39:48 +0100

    Add forward facing PostCSS plugins to runner

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -4,12 +4,15 @@ var changed = require('gulp-changed');
 var bs = require('browser-sync').get('bs');
 var postcss = require('gulp-postcss');
 var nano = require('gulp-cssnano');
-var autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function () {
   return gulp.src(config.src)
     .pipe(changed(config.dest))
     .pipe(postcss([
+      require('postcss-import'),
+      require('postcss-custom-properties'),
+      require('postcss-calc'),
+      require('postcss-custom-media'),
       require('autoprefixer')
     ]))
     .pipe(nano())
