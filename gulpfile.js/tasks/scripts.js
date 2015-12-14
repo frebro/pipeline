0361062dd9b37380b6d70d480de088f0808da122
Fredrik Broman <frebro@gmail.com>
2015-12-14 23:22:43 +0100

    Run scripts through ESLint + Babel

--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -1,28 +1,21 @@
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-var jshint = require('gulp-jshint');
-var notify = require('gulp-notify');
-var uglify = require('gulp-uglify');
-var bs = require('browser-sync').get('bs');
-var config = require('../config/scripts');
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+const eslint = require('gulp-eslint');
+const babel = require('gulp-babel');
+const bs = require('browser-sync').get('bs');
+const config = require('../config/scripts');
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['scripts:lint'], function () {
   return gulp.src(config.src)
     .pipe(changed(config.dest))
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(notify(function (file) {
-      if (file.jshint.success) {
-        return false;
-      }
-      var errors = file.jshint.results.map(function (data) {
-        if (data.error) {
-          return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
-        }
-      }).join("\n");
-      return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
-    }))
-    .pipe(uglify())
+    .pipe(babel())
     .pipe(gulp.dest(config.dest))
     .pipe(bs.stream());
+});
+
+gulp.task('scripts:lint', function() {
+  return gulp.src(config.src)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
